2dac3ecdd6c1d265d40f03bfe080872a0a0219a2
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-12 13:55:00 +1200

    task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2){
+    Console.WriteLine("Максимальное первое число " + num1);
+}
+else if (num2>num1){
+    Console.WriteLine("Максимальное второе число " + num2);
+}
+if (num1<num2){
+    Console.WriteLine("Минимальное первое число " + num1);
+}
+else if (num2<num1){
+    Console.WriteLine("Минимальное второе число " + num2);
+}
